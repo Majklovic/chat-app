b225601ff6e05b20bf3db887f845f99795498a86
Majklovic <michalmissbach@seznam.cz>
2025-09-02 00:45:39 +0200

    saving progres

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Login.css';
+import assets from '../../assets/assets';
+
+const Login = () => {
+  return (
+    <div className='login'>
+      <img src={assets.logo_big} alt="" className='logo'/>
+      <form className="login-form">
+        <h2>Sign Up</h2>
+        <input type="text" placeholder="username" className='form-input' required/>
+        <input type="email" placeholder="Email address" className='form-input' />
+        <input type="password" placeholder="password" className='form-input' />
+      </form>
+    </div>
+  );
+}
+
+export default Login;
