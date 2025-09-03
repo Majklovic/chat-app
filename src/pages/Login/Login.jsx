b3438be3e599c4cac6f1276440e44f83238a20e5
Majklovic <michalmissbach@seznam.cz>
2025-09-03 23:37:05 +0200

    update

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -9,8 +9,17 @@ const Login = () => {
       <form className="login-form">
         <h2>Sign Up</h2>
         <input type="text" placeholder="username" className='form-input' required/>
-        <input type="email" placeholder="Email address" className='form-input' />
-        <input type="password" placeholder="password" className='form-input' />
+        <input type="email" placeholder="Email address" className='form-input' required/>
+        <input type="password" placeholder="password" className='form-input' required/>
+        <button type='submit'>Sign Up</button>
+        <div className='login-term'>
+          <input type="checkbox" />
+          <p>Agree to the terms of use & privacy policy</p>
+        </div>
+        <div className="login-forgot">
+          <p className="login-toggle">Already have an account <span>click here</span></p>
+
+        </div>
       </form>
     </div>
   );
